5a1605121f245c999f89d644f333ccc5dcd20f27
Kieran Harrison <17HarriK@fulford.york.sch.uk>
2022-09-23 15:13:19 +0100

    Added validation

--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -10,7 +10,40 @@ namespace Pizza
     {
         static void Main(string[] args)
         {
+            List<string> toppings = new List<string> {
+                "Pepperoni","Margherita","Ham and pineapple"
+        };
 
+            string selection = ChooseFrom(toppings);
+            Console.WriteLine($"You have chosen {selection}");
+        }
+
+        private static string ChooseFrom(List<string> toppings)
+        {
+         
+            bool valid = false;
+            int chosenPosition = -1;
+
+            while (valid == false)
+            {
+                for (int i = 0; i < toppings.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {toppings[i]}");
+                }
+
+                Console.Write($"Choose a number (1-{toppings.Count}) ");
+                string response = Console.ReadLine();
+
+                if (int.TryParse(response, out chosenPosition) &&
+                    chosenPosition > 0 &&
+                    chosenPosition <= toppings.Count)
+                {
+                    valid = true;
+                }
+
+            }
+
+            return toppings[chosenPosition - 1];
         }
     }
 }
